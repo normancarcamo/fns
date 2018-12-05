bf64992e82baa466f22f2783f998282ab3e76539
Norman Carcamo <normancarcamo@gmail.com>
2018-12-04 23:14:39 -0600

    added "to" module

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const deepSearch = require('deep-object-search');
 const promises = require('./lib/promises');
 const various = require('./lib/various');
 const is = require('./lib/is');
+const to = require('./lib/to');
 
 const fns = {
   is,
-  deepSearch,
+  to,
   ...promises,
-  ...various
+  ...various,
+  deepSearch,
 };
 
 module.exports = fns;
