c7cd2239894551379e2fdaf55147a3d4ddda3f3c
Norman Carcamo <normancarcamo@gmail.com>
2018-12-14 22:04:46 -0600

    Added lib/get.js to index.js file to be exposed when the package is required

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const deepSearch = require('deep-object-search');
 const promises = require('./lib/promises');
 const various = require('./lib/various');
+const get = require('./lib/get');
 const errors = require('./lib/errors');
 const is = require('./lib/is');
 const to = require('./lib/to');
@@ -10,6 +11,7 @@ const middlewares = require('./lib/middlewares');
 const fns = {
   is,
   to,
+  get,
   ...promises,
   ...various,
   deepSearch,
